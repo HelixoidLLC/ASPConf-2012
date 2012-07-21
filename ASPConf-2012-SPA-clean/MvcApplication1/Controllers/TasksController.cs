75af0b1c8bef8aed05ccc7244a7680b0951bb2e7
Igor <igorm@bluemetal.com>
2012-07-20 21:41:29 -0400

    Clean application sckeleton ready for a demo

---
+++ ASPConf-2012-SPA-clean/MvcApplication1/Controllers/TasksController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class TasksController : ApiController
+    {
+        public void Post(Task task)
+        {
+            TasksRepository.Add(task);
+        }
+
+        public IEnumerable<Task> Get()
+        {
+            return TasksRepository.GetAll();
+        }
+    }
+}
---
+++ ASPConf-2012-SPA-clean/MvcApplication1/Models/TasksRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace MvcApplication1.Models
+{
+    public static class TasksRepository
+    {
+        private static readonly List<Task> Tasks = new List<Task>();
+
+        static TasksRepository()
+        {
+            Tasks.Add(new Task { Subject = "Vacuum floors"});
+            Tasks.Add(new Task { Subject = "Water plants" });
+        } 
+
+        public static IEnumerable<Task> GetAll()
+        {
+            return Tasks;
+        }
+
+        public static void Add(Task task)
+        {
+            Tasks.Add(task);
+        }
+    }
+}
